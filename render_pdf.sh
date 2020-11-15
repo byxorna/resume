f78709ad1e27edfe7b9effa04fc7aa760dea8de6
Gabe Conradi <gabe.conradi@gmail.com>
2020-11-15 10:09:36 -0500

    better commit representation

--- render_pdf.sh
+++ render_pdf.sh
@@ -4,7 +4,8 @@ if [[ ! -x node_modules/.bin/mdpdf ]] ; then
   yarn install
 fi
 
-commit="$(git rev-parse --short=7 HEAD)"
+commit="$(git rev-parse HEAD)"
+commit_short="$(git rev-parse --short=7 HEAD)"
 tag="$(git describe --tags --always)"
 
 header="$(mktemp)"
@@ -12,7 +13,7 @@ footer="$(mktemp)"
 style="$(mktemp)"
 cat >$footer <<_EOF_
 <div style="font-size: 8px; text-align: right;float:right;width:100%;margin-right:8px;">
-  <span><a href="https://github.com/byxorna/resume/commit/$commit">byorna/resume@$tag</a> (<a href="https://github.com/byxorna/resume/commit/$commit">$commit</a>) $(date -u)</span>
+  <span><a href="https://github.com/byxorna/resume/commit/$commit">byorna/resume@$tag</a> (<a href="https://github.com/byxorna/resume/commit/$commit">$commit_short</a>) $(date -u)</span>
 </div>
 _EOF_
 
